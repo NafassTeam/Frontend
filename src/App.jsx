ac2bb0e585091255cafda1e58c6a1d2187161b0f
mira <a.laichaoui@esi-sba.dz>
2025-04-20 21:43:58 +0100

    Finished email verification and styling update done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import AppRoutes from './Routes/AppRoutes';
 import './App.css'
-import EmailVerified from "/src/Pages/EmailVerified.jsx";
-
 
 const App = () => {
-  return <EmailVerified />;
+  return <AppRoutes />;
 };
 
 export default App;
